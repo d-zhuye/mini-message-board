8359aafabe6769d51e8442b38cda0671ccf5ac39
Zhu Ye Deng <180813277+d-zhuye@users.noreply.github.com>
2025-05-24 23:16:08 -0700

    feat: add router.post() to receive form submit

--- routes/newMessageRouter.js
+++ routes/newMessageRouter.js
@@ -1,14 +1,26 @@
 import { Router } from "express";
 import { getAllCountries } from "../util.js";
+import { messages } from "../db.js";
 
 const newMessageRouter = Router();
 
-newMessageRouter.get("/new", async (req, res) => {
+newMessageRouter.get("/", async (req, res) => {
   const countries = await getAllCountries();
 
   res.render("../views/newMessage.ejs", { countries });
 });
 
-newMessageRouter.post("/new", (req, res) => {});
+newMessageRouter.post("/", (req, res) => {
+  console.log(req.body);
+  const currTime = new Date();
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.messageName,
+    country: req.body.messageCountry,
+    added: currTime,
+  });
+
+  res.redirect("/");
+});
 
 export { newMessageRouter };
